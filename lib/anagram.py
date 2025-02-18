1f8a402e9e057f74d283534a736c5b0ac8458331
irarubrian <brian.o.iraru@gmail.com>
2025-02-18 15:59:36 +0300

    commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower() 
+
+    def match(self, words):
+        return [w for w in words if sorted(w.lower()) == sorted(self.word) and w.lower() != self.word]
+
+# Example usage:
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))  # Output: ['inlets']
